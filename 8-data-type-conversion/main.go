075745beb113d608d1d123ee3389041ea98a4f19
alfianchii <pianaee379@gmail.com>
2025-05-21 19:51:25 +0700

    8-feat: ASCII to string

--- 8-data-type-conversion/main.go
+++ 8-data-type-conversion/main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+	// Int
 	var value1 int32 = 32768
 	var value2 int64 = int64(value1)
 	// Number overflow
@@ -11,4 +12,13 @@ func main() {
 	fmt.Println(value1)
 	fmt.Println(value2)
 	fmt.Println(value3)
+
+	// String
+	var name = "Alfian Taka"
+	var firstCharOfName = name[0] // ASCII
+	var firstCharOfNameString = string(firstCharOfName) // ASCII to string
+	
+	fmt.Println(name)
+	fmt.Println(firstCharOfName)
+	fmt.Println(firstCharOfNameString)
 }
