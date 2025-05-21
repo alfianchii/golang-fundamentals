f5b571aa5787f47a51fbbeaa85643ec0d4cd3389
alfianchii <pianaee379@gmail.com>
2025-05-21 16:29:34 +0700

    5-feat: str len and array

--- 5-string-data-type/main.go
+++ 5-string-data-type/main.go
@@ -5,4 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("Alfian")
 	fmt.Println("Alfian Taka")
+	fmt.Println(len("Taka"))
+	fmt.Println("Alfian"[0]) // ASCII code
+	fmt.Println("Alfian Taka"[1])
 }
