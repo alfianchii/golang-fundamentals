172c6abfb3531c1051fc6d1da5768b1e55f8f070
alfianchii <pianaee379@gmail.com>
2025-05-22 18:56:08 +0700

    14-feat: slice built-in funcs

--- 14-slice-data-type/main.go
+++ 14-slice-data-type/main.go
@@ -16,5 +16,45 @@ func main() {
 	fmt.Println("Second slice:", secondSlice)
 	fmt.Println("Third slice:", thirdSlice)
 	fmt.Println("Fourth slice:", fourthSlice)
-	fmt.Println("========================================================")
+	fmt.Println("================================================================================================================")
+
+	// Funcs
+	days := [7]string{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"}
+	fmt.Println("Days:", days)
+	fmt.Println("----------------")
+
+	workDays := days[:5]
+	workDaysLength := len(workDays)
+	workDaysCapacity := cap(workDays)
+	fmt.Println("Work days:", workDays)
+	fmt.Println("Work days length:", workDaysLength)
+	fmt.Println("Work days capacity:", workDaysCapacity)
+	fmt.Println("----------------")
+
+	holiday1 := "Holiday 1"
+	holiday2 := "Holiday 2"
+	workDays[0] = holiday1
+	workDays[1] = holiday2
+	fmt.Println("Replace some old data to 'workDays':", workDays) // Changed
+	fmt.Println("The 'days' data:", days) // Also affected into the parent
+	fmt.Println("----------------")
+
+	workDay1 := "Workday 1"
+	workDay2 := "Workday 2"
+	/*
+		append() also added the data to its parent, 
+		but start from the end of the sliced data's index (the reference).
+		WOULDN'T add the data into the reference (workDays)
+	*/ 
+	weekend := append(workDays, workDay1)
+	fmt.Println("Weekend:", weekend)
+	fmt.Println("Workdays:", workDays)
+	fmt.Println("Days:", days)
+	// And affected into the reference and parent too
+	fmt.Println("----------------CHANGE THE weekend[0]!----------------")
+	weekend[0] = workDay2
+	fmt.Println("Weekend:", weekend)
+	fmt.Println("Workdays:", workDays)
+	fmt.Println("Days:", days)
+	fmt.Println("================================================================================================================")
 }
