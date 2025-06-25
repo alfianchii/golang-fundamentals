09dac9909087c5b86aa6a092484c98dd43b218ab
alfianchii <pianaee379@gmail.com>
2025-06-25 21:38:35 +0700

    15-feat: map's funcs

--- 15-map-data-type/main.go
+++ 15-map-data-type/main.go
@@ -19,4 +19,15 @@ func main() {
 	fmt.Println(person)
 	fmt.Println(person["name"])
 	fmt.Println(person["address"])
+
+	// Funcs
+	var book map[string]string = make(map[string]string)
+	book["title"] = "Journey of My Life"
+	book["author"] = "Alfian Taka"
+	book["apkhBtul"] = "ia"
+
+	delete(book, "apkhBtul")
+
+	fmt.Println(book)
+	fmt.Println(len(book))
 }
