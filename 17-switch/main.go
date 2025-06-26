396a14c7afcbfd8241ed013bf83184c52eed3efd
alfianchii <pianaee379@gmail.com>
2025-06-26 20:52:57 +0700

    17-feat: switch short statement

--- 17-switch/main.go
+++ 17-switch/main.go
@@ -17,4 +17,11 @@ func main() {
 	default:
 		fmt.Println("Hello! What's ur name?")
 	}
+
+	switch nameLength := len(name); nameLength > 5 {
+	case true:
+		fmt.Println("Your name is too long.")
+	case false:
+		fmt.Println("Your name is OK.")
+	}
 }
