fa419d9b64ebe1e0322fe32dad12fe2de86db2b3
alfianchii <pianaee379@gmail.com>
2025-06-26 20:55:28 +0700

    17-feat: switch without condition

--- 17-switch/main.go
+++ 17-switch/main.go
@@ -18,10 +18,22 @@ func main() {
 		fmt.Println("Hello! What's ur name?")
 	}
 
+	// Short
 	switch nameLength := len(name); nameLength > 5 {
 	case true:
 		fmt.Println("Your name is too long.")
 	case false:
+		fmt.Println("Your name is OK.")
+	}
+
+	// Without condition (better use if)
+	length := len(name);
+	switch {
+	case length > 10:
+		fmt.Println("Your name is very long.")
+	case length > 5:
+		fmt.Println("Your name is too long.")
+	default: 
 		fmt.Println("Your name is OK.")
 	}
 }
