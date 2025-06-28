73796eb8695d026843cfd73859dca413372c52e7
alfianchii <pianaee379@gmail.com>
2025-06-28 09:40:35 +0700

    31-feat: panic

--- 31-defer-panic-n-recover/main.go
+++ 31-defer-panic-n-recover/main.go
@@ -2,15 +2,23 @@ package main
 
 import "fmt"
 
+func logEndApp() {
+	fmt.Println("End the app!")
+}
+
 func logging() {
 	fmt.Println("App is closed!")
 }
 
-func runApp() {
+func runApp(error bool) {
 	defer logging()
+	defer logEndApp()
 	fmt.Println("Run the app...")
+	if error {
+		panic("ERROR OCCURS!")
+	}
 }
 
 func main() {
-	runApp()
+	runApp(true)
 }
