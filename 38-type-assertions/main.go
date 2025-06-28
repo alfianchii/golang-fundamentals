bd3434b0c257eb4fd2596ca7fccfe9e2a2b6964c
alfianchii <pianaee379@gmail.com>
2025-06-28 20:29:18 +0700

    39-feat: type assertions w/ switch statement

--- 38-type-assertions/main.go
+++ 38-type-assertions/main.go
@@ -19,7 +19,18 @@ func main() {
 	var resultInInt int = result2.(int)
 	fmt.Println(resultInInt)
 
-	// Panic occurs
-	var data int = result.(int)
-	fmt.Println(data)
+	// // Panic occurs
+	// var data int = result.(int)
+	// fmt.Println(data)
+
+	// Type Assertions w/ switch statement
+	var data interface{} = thisReturnsInt()
+	switch value := data.(type) {
+	case string:
+		fmt.Println("This is a 'string' data type:", value)
+	case int:
+		fmt.Println("This is a 'int' data type:", value)
+	default:
+		fmt.Println("Unknown:", value)
+	}
 }
