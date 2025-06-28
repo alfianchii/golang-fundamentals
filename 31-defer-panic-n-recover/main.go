5762bfbc0bf87110ffd1b3de7d950f890881dd3c
alfianchii <pianaee379@gmail.com>
2025-06-28 09:47:51 +0700

    31-feat: recover

--- 31-defer-panic-n-recover/main.go
+++ 31-defer-panic-n-recover/main.go
@@ -3,22 +3,26 @@ package main
 import "fmt"
 
 func logEndApp() {
-	fmt.Println("End the app!")
+	fmt.Println("End the app...")
+	msg := recover()
+	fmt.Println("Error occurs:", msg)
 }
 
 func logging() {
-	fmt.Println("App is closed!")
+	fmt.Println("App is closed.")
 }
 
 func runApp(error bool) {
+	// Deferred funcs run in LIFO Order
 	defer logging()
 	defer logEndApp()
 	fmt.Println("Run the app...")
 	if error {
-		panic("ERROR OCCURS!")
+		panic("WOOPS!")
 	}
 }
 
 func main() {
 	runApp(true)
+	fmt.Println("We passed the panic disaster!")
 }
