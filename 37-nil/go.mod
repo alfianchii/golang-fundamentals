814b0cedf4092ec60da3d38e5d39b52de6020761
alfianchii <pianaee379@gmail.com>
2025-06-28 20:10:00 +0700

    37-feat: nil

---
+++ 37-nil/go.mod
@@ -0,0 +1,3 @@
+module 37-nil
+
+go 1.23.3
---
+++ 37-nil/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+/*
+	nil can only be used w/ certain data types,
+	such as interface, func, map, slice, pointer,
+	and channel.
+*/
+
+// func ItCouldNotWorks(name string) string {
+// 	if name == "" {
+// 		return nil
+// 	} else {
+// 		return name
+// 	}
+// }
+
+func NewMap(name string) map[string]string {
+	if name == "" {
+		return nil
+	} else {
+		return map[string]string{
+			"name": name,
+		}
+	}
+}
+
+func main() {
+	var taka map[string]string = NewMap("Taka")
+	if taka == nil {
+		fmt.Println("Data is empty.")
+	} else {
+		fmt.Println(taka["name"])
+	}
+
+	var moe map[string]string = NewMap("")
+	if moe == nil {
+		fmt.Println("Data is empty.")
+	} else {
+		fmt.Println(moe["name"])
+	}
+}
