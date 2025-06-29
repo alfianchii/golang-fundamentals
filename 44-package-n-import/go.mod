1cc039cf18a255390e7effb2705bdb6ff993418a
alfianchii <pianaee379@gmail.com>
2025-06-29 13:55:19 +0700

    44-feat: package n import

---
+++ 44-package-n-import/go.mod
@@ -0,0 +1,3 @@
+module 44-package-n-import
+
+go 1.23.3
---
+++ 44-package-n-import/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"44-package-n-import/helper"
+	"fmt"
+)
+
+func main() {
+	var result string = helper.SayHello("Taka")
+
+	fmt.Println(result)
+}
